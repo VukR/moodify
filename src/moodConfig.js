b3a05d642c676414d2658453707a841f73a53668
VukR <vuk@ualberta.ca>
2023-08-23 10:37:38 +0200

    Add mock songs to config

--- src/moodConfig.js
+++ src/moodConfig.js
@@ -6,3 +6,108 @@ export const MOODS = [
   { type: "Canadian", image: "canada.png" },
   { type: "Relaxed", image: "spa.png" },
 ];
+
+export const SONGS = {
+  Joyful: [
+    {
+      name: "Walking On Sunshine",
+      artist: "Katrina And The Waves",
+      url: "https://www.youtube.com/watch?v=qK5KhQG06xU",
+    },
+    {
+      name: "Girls Just Want To Have Fun",
+      artist: "Cyndi Lauper",
+      url: "https://www.youtube.com/watch?v=9fiRtYcjhJ4",
+    },
+    {
+      name: "Dancing Queen",
+      artist: "ABBA",
+      url: "https://www.youtube.com/watch?v=3qiMJt-JBb4",
+    },
+  ],
+  "Hans Zimmer": [
+    {
+      name: "Time (Inception)",
+      artist: "Hans Zimmer",
+      url: "https://www.youtube.com/watch?v=c56t7upa8Bk",
+    },
+    {
+      name: "One Day (Pirate of the Carribean)",
+      artist: "Hans Zimmer",
+      url: "https://www.youtube.com/watch?v=cScVoZWVDLQ",
+    },
+    {
+      name: "S.T.A.Y (Interstellar)",
+      artist: "Hans Zimmer",
+      url: "https://www.youtube.com/watch?v=Ia3eQ7QD9Z0",
+    },
+  ],
+  Study: [
+    {
+      name: "Three Little Birds",
+      artist: "Bob Marley",
+      url: "https://www.youtube.com/watch?v=LanCLS_hIo4",
+    },
+    {
+      name: "Eye Of The Tiger",
+      artist: "Survivor",
+      url: "https://www.youtube.com/watch?v=_qDML_BCju8",
+    },
+    {
+      name: "Survivor",
+      artist: "Destiny's Child",
+      url: "https://www.youtube.com/watch?v=lXAdAa4ZuuY",
+    },
+  ],
+  Memes: [
+    {
+      name: "All Star",
+      artist: "Smash Mouth",
+      url: "https://www.youtube.com/watch?v=aT5JaB5agSE",
+    },
+    {
+      name: "Never Gonna Give You Up",
+      artist: "Rick Astley",
+      url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    },
+    {
+      name: "Crab Rave",
+      artist: "Noisestorm",
+      url: "https://www.youtube.com/watch?v=-50NdPawLVY",
+    },
+  ],
+  Canadian: [
+    {
+      name: "My Heart Will Go On",
+      artist: "Celine Dion",
+      url: "https://www.youtube.com/watch?v=Qf4zY6diTwQ",
+    },
+    {
+      name: "Hotline Bling",
+      artist: "Drake",
+      url: "https://www.youtube.com/watch?v=LxxnSDmGUc4",
+    },
+    {
+      name: "Stay",
+      artist: "Justin Bieber ft The Kid Laroi",
+      url: "https://www.youtube.com/watch?v=yWHrYNP6j4k",
+    },
+  ],
+  Relaxed: [
+    {
+      name: "Heavy Rain & Thunder",
+      artist: "Danny Louis",
+      url: "https://www.youtube.com/watch?v=Of3wJ8fWJQ0",
+    },
+    {
+      name: "Weightless",
+      artist: "Marconi Union",
+      url: "https://www.youtube.com/watch?v=UfcAVejslrU",
+    },
+    {
+      name: " Please Don't Go ",
+      artist: "Barcelona",
+      url: "https://www.youtube.com/watch?v=-kizV91zQ_0",
+    },
+  ],
+};
